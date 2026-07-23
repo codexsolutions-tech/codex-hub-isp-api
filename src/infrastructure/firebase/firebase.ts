3e67a564526868e1e769cc4575f9b12433ff4dc1
codexsolutions-tech <codexsolutions9@gmail.com>
2026-07-22 21:25:48 -0300

    fix

--- src/infrastructure/firebase/firebase.ts
+++ src/infrastructure/firebase/firebase.ts
@@ -1,6 +1,6 @@
-import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+/*import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-/* import serviceAccount from './chave/hubisp-firebase-adminsdk-fbsvc-826bf254dd.json';
+import serviceAccount from './chave/hubisp-firebase-adminsdk-fbsvc-826bf254dd.json';
 
 export const firebaseApp = initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
