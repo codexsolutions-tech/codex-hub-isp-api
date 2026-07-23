01c08b16d8b877139a9e456bf375534eb399da88
codexsolutions-tech <codexsolutions9@gmail.com>
2026-07-22 21:06:10 -0300

    fix: referencia

--- src/infrastructure/firebase/firebase.ts
+++ src/infrastructure/firebase/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import serviceAccount from './chave/hubisp-firebase-adminsdk-fbsvc-826bf254dd.json';
 
+
 export const firebaseApp = initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
 });
